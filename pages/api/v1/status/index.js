be6a9c8316279612e7e62c39dd6330a1173674ba
menegallidev <menegallidev@gmail.com>
2026-01-06 20:15:00 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).send("Sistema UP!");
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const currentConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        current_connections: currentConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
